27965bad4569e96da099640e97871f292c7e38ad
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-05-04 18:25:03 +0800

    Add click event to menu and contact button in index page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-import './style.css';
+import './reset.css';
+import './common.css';
 import { renderLanding } from './landing-page.js';
+import { renderMenu } from './menu.js';
 
-renderLanding();
+const render =(() => {
+    const getContainer = document.querySelector("#content");
+    const getBtn = document.querySelectorAll("button");
+    renderLanding();
+
+    const getAbout = document.querySelector(".about");
+    getAbout.addEventListener("click", () => {
+        getBtn.forEach((button) => button.classList.remove("active"));
+        getContainer.innerHTML = "";
+        renderLanding();
+    });
+
+    const getMenu = document.querySelector(".menu");
+    getMenu.addEventListener("click", (e) => {
+        getBtn.forEach((button) => button.classList.remove("active"));
+        getMenu.classList.add("active");
+        getContainer.innerHTML = "";
+        renderMenu();
+    })
+
+    const getContact = document.querySelector(".contact");
+    getContact.addEventListener("click", () => {
+        getBtn.forEach((button) => button.classList.remove("active"));
+        getContact.classList.add("active");
+        getContainer.innerHTML = "";
+    })
+})();
 
